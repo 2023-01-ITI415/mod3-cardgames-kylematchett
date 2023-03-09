0b0c2352e29b33d746ccfb3fafa4b811fe35d8d9
kylematchett <113548697+kylematchett@users.noreply.github.com>
2023-03-08 21:22:08 -0500

    completed cards

--- Assets/Prospector/__Scripts/Deck.cs
+++ Assets/Prospector/__Scripts/Deck.cs
@@ -87,7 +87,7 @@ public class Deck : MonoBehaviour
         GameObject go = Instantiate<GameObject>(prefabCard, deckAnchor); // f
         Card card = go.GetComponent<Card>();
 
-        //card.Init(suit, rank, startFaceUp);
+        card.Init(suit, rank, startFaceUp);
         // g
         return card;
     }
